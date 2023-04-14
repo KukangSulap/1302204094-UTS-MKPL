0c72ecfa7b8b26f0158968d71dcb9de56ae30e36
Howler Nofelle <hilmy@wahidin.net>
2023-04-14 21:06:45 +0700

    if else statement dikurangi di function calculateTax dan ditambahkan variabel baru yaitu taxMain

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -12,30 +12,25 @@ public class TaxFunction {
 	 * - memiliki anak maka ditambah sebesar Rp4.500.000 per anak sampai anak ketiga.
 	 */
 
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
+	public static int calculateTax(
+			int monthlySalary,
+			int otherMonthlyIncome,
+			int numberOfMonthWorking,
+			int deductible,
+			boolean isMarried,
+			int numberOfChildren
+	) {
+
+		int taxMain = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking - deductible;
+
+		if (numberOfMonthWorking > 12 || numberOfChildren > 3) {
+			System.err.println("More than 12 month working per year or 3 children");
 			return 0;
-		}else {
-			return tax;
 		}
-			 
+
+		int taxFree = 54000000 + (isMarried ? 4500000 : 0) + (numberOfChildren * 1500000);
+		int taxableIncome = Math.max(taxMain - taxFree, 0);
+		return (int) Math.round(0.05 * taxableIncome);
 	}
 	
 }
