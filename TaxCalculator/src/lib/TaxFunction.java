5475c995a80a8ece4bba627d098f919d75327d1b
Howler Nofelle <hilmy@wahidin.net>
2023-04-14 18:59:37 +0700

    Comment di TaxFunction sudah diperbaiki agar lebih mudah dibaca

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -2,19 +2,16 @@ package lib;
 
 public class TaxFunction {
 
-	
 	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
+	 * Fungsi menghitung pajak tahunan dengan rumus 5% dari penghasilan bersih tahunan.
+	 * Penghasilan bersih diperoleh dengan mengurangi penghasilan bulanan dengan pemotongan dan
+	 * dikali dengan jumlah bulan dalam setahun.
+	 *
+	 * - belum menikah atau memiliki anak maka tidak kena pajak, jadi Rp54.000.000
+	 * - sudah menikah maka ditambah sebesar Rp4.500.000
+	 * - memiliki anak maka ditambah sebesar Rp4.500.000 per anak sampai anak ketiga.
 	 */
-	
-	
+
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
 		int tax = 0;
